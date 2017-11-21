95e91a672194d45a6a56a7da9b305ccdd2dfd389
Brooks <jianyu996@163.com>
2017-11-21 22:42:53 +0800

    demo

--- BTTask/ViewController.m
+++ BTTask/ViewController.m
@@ -8,6 +8,9 @@
 
 #import "ViewController.h"
 
+#import "BTTask.h"
+#import "Task1.h"
+
 @interface ViewController ()
 
 @end
@@ -15,7 +18,27 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];    
+    [super viewDidLoad];
+    
+    // 直接使用
+    BTTask *task = [BTTask taskWithBlock:^NSError *(BTTask *task) {
+        /**
+         TODO
+         
+         */
+        
+        NSLog(@"BTTask test");
+        return nil;
+    }];
+    
+    // 继承，Task1 继承自 BTTask
+    Task1 *task1 = [[Task1 alloc] init];
+    
+    [task1 addDependency:task];
+    
+    NSOperationQueue *queue = [[NSOperationQueue alloc ] init];
+    [queue addOperations:@[task,task1] waitUntilFinished:NO];
+    
 }
 
 
